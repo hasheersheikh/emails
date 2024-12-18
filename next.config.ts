ae6b150ee5dbb997156ce9be71fad3d65ed1e257
Hashir Sheikh <hasheersheikh@gmail.com>
2024-12-18 14:33:08 +0530

    fixed next.config.js file

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
